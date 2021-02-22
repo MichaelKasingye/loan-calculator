4340a18632ebc468753f6e616626fc442c813cd8
MichaelKasingye <michaelkasingye@gmail.com>
2021-02-22 05:58:04 -0800

    add pwa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Summary from "./Pages/Summary";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/summary" component={Summary}/>
-            <Route path="/"component={Home}/>
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/summary" component={Summary}/>
+          <Route exact path="/"component={Home}/>
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
   );
 }
 
